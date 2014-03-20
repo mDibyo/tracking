eb96dc9dbe0bbe79f7e8f70ebf0130b56531de4d
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-03-19 19:26:25 -0700

    Completed observation emission update

--- inference.py
+++ inference.py
@@ -154,16 +154,16 @@ class ExactInference(InferenceModule):
     # Be sure to handle the "jail" edge case where the ghost is eaten
     # and noisyDistance is None
     allPossible = util.Counter()
-    for p in self.legalPositions:
-      trueDistance = util.manhattanDistance(p, pacmanPosition)
-      if emissionModel[trueDistance] > 0:
-        allPossible[p] = 1.0
+    if noisyDistance:
+      for p in self.legalPositions:
+        trueDistance = util.manhattanDistance(p, pacmanPosition)
+        if emissionModel[trueDistance] > 0:
+          allPossible[p] = emissionModel[trueDistance] * self.beliefs[trueDistance]
     # handling of jail edge case
-    if not noisyDistance:
+    else:
       for p in self.legalPositions:
         allPossible[p] = 0.0
       allPossible[self.getJailPosition()] = 1.0
-
     allPossible.normalize()
     self.beliefs = allPossible
 
